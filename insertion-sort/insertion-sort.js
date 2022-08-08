70cffe48be5b0a5a3e26b455454f434e7d873b81
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-08 11:59:10 +0545

    Add: insertion sort

---
+++ insertion-sort/insertion-sort.js
@@ -0,0 +1,18 @@
+
+function insertionSort(arr) {
+  const _arr = [...arr];
+  let marker = 0;
+  let current = 1;
+  while (marker <= arr.length) {
+    for (let i = marker; i >= 0; i--) {
+      if (_arr[current] < _arr[i]) {
+        [_arr[current], _arr[i]] = [_arr[i], _arr[current]];
+        current--;
+      }
+    }
+    current = ++marker;
+  }
+  return _arr;
+}
+
+module.exports = insertionSort;
